0fe46872f77727e0b0ddf744413a634efc80cf9d
ZiporaFalk <z0548498935@gmail.com>
2025-07-23 17:17:17 +0300

    Replacing-existing-audio

---
+++ FFmpeg.Infrastructure/Commands/AudioReplaceCommand .cs
@@ -0,0 +1,33 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class AudioReplaceCommand: BaseCommand, ICommand<AudioReplaceModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+        public AudioReplaceCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder;
+        }
+        public async Task<CommandResult> ExecuteAsync(AudioReplaceModel model)
+        {
+            CommandBuilder = _commandBuilder
+                .SetInput(model.VideoFile) // input.mp4
+                .SetInput(model.NewAudioFile) // new_audio.mp3
+                .AddOption("-c:v copy")
+                .AddOption("-map 0:v:0")
+                .AddOption("-map 1:a:0")
+                .SetOutput(model.OutputFile);
+
+            return await RunAsync();
+        }
+    }
+}
