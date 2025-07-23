bcd6990c12c604e44cf39257d0ae3dac0ce4c2c3
BS885 <bitya32885@gmail.com>
2025-07-23 16:23:36 +0300

    add dto

---
+++ Ffmpeg.API/DTOs/TimestampDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FFmpeg.API.DTOs
+{
+    public class TimestampDto
+    {
+        public IFormFile VideoFile { get; set; }
+        public int XPosition { get; set; } = 10;
+        public int YPosition { get; set; } = 10;
+        public int FontSize { get; set; } = 24;
+        public string FontColor { get; set; } = "white";
+    }
+}
