e7844b82df66c4a1290beb2c43953dacd600d55c
BS885 <bitya32885@gmail.com>
2025-07-23 11:00:54 +0300

    Add timestamp overlay command and model

---
+++ FFmpeg.Infrastructure/Commands/TimestampCommand.cs
@@ -0,0 +1,43 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class TimestampCommand : BaseCommand, ICommand<TimestampModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public TimestampCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(TimestampModel model)
+        {
+            string drawtextFilter = $"drawtext=text='%{{pts\\:hms}}':x={model.XPosition}:y={model.YPosition}:fontsize={model.FontSize}:fontcolor={model.FontColor}";
+
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddFilterComplex(drawtextFilter)
+                .AddOption($"-map 0:a?")
+                .AddOption($"-c:a copy");
+
+            if (model.IsVideo)
+            {
+                CommandBuilder.SetVideoCodec(model.VideoCodec);
+            }
+
+            CommandBuilder.SetOutput(model.OutputFile, model.IsVideo ? false : true);
+
+            return await RunAsync();
+        }
+    }
+
+}
