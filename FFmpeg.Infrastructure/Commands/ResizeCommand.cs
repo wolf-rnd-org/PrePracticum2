df694424de6c896d7298a847bde1d26e79b724de
bracha-shreiber <b0583270458@gmail.com>
2025-07-27 01:17:38 +0300

    Change resolution

---
+++ FFmpeg.Infrastructure/Commands/ResizeCommand.cs
@@ -0,0 +1,37 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class ResizeCommand : BaseCommand, ICommand<ResizeModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public ResizeCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(ResizeModel model)
+        {
+            string scaleFilter = $"scale={model.Width}:{model.Height}";
+
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddOption($"-vf {scaleFilter}")
+                .SetVideoCodec(model.VideoCodec)
+                .SetOutput(model.OutputFile);
+
+            return await RunAsync();
+        }
+    }
+}
+
+
