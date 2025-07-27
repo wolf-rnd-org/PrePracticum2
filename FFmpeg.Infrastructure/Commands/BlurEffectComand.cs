5958dae4510bae9156021dd5f23a8cff7b7e854c
shifi-s <s0548493359@gmail.com>
2025-07-27 14:40:47 +0300

    removing blank lines

--- FFmpeg.Infrastructure/Commands/BlurEffectComand.cs
+++ FFmpeg.Infrastructure/Commands/BlurEffectComand.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace FFmpeg.Infrastructure.Commands
 {
     public class BlurEffectComand(FFmpegExecutor executor, ICommandBuilder commandBuilder) : BaseCommand(executor), ICommand<BlurEffectModel>
@@ -18,8 +17,6 @@ namespace FFmpeg.Infrastructure.Commands
             CommandBuilder = _commandBuilder.SetInput(model.VideoName)
                 .SetOutput(model.OutputName)
                  .AddOption($"-vf \"gblur=sigma={model.Sigma}\"");
-               
-
             return RunAsync();
         }
     }
