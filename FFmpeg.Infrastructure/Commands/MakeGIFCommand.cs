427cde14e81601638a111787051aec39f5d7d56a
Tovi Yakovson <zivyaya865@gmail.com>
2025-07-27 12:32:47 +0300

    add GIFModel and the 'create gif from video' class as 'MakeGIFCommand'

---
+++ FFmpeg.Infrastructure/Commands/MakeGIFCommand.cs
@@ -0,0 +1,37 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Infrastructure.Services;
+using FFmpeg.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class MakeGIFCommand : BaseCommand, ICommand<GIFModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public WatermarkCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(GIFModel model)
+        {
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddOption("-vf \"fps=10,scale=320:-1\"")
+                .SetOutput(model.OutputGifName);
+            if (model.IsVideo)
+            {
+                CommandBuilder.SetVideoCodec(model.VideoCodec);
+            }
+
+            return await RunAsync();
+        }
+
+    }
+}
