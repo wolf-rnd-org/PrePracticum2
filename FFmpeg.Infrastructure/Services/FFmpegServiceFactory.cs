f1a631d6f5a0d54409934e212b127f961054b060
bracha-shreiber <b0583270458@gmail.com>
2025-07-28 22:45:42 +0300

    upload-change-resolution

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -17,6 +17,8 @@ namespace FFmpeg.Infrastructure.Services
         ICommand<ReplaceAudioModel> CreateReplaceAudioCommand();
         ICommand<TimestampModel> CreateTimestampCommand();
         ICommand<ConvertAudioModel> CreateConvertAudioCommand();
+        ICommand<ResizeModel> CreateResizeCommand();
+
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -50,6 +52,10 @@ namespace FFmpeg.Infrastructure.Services
         public ICommand<ConvertAudioModel> CreateConvertAudioCommand()
         {
             return new ConvertAudioCommand(_executor, _commandBuilder);
+        }
+        public ICommand<ResizeModel> CreateResizeCommand()
+        {
+            return new ResizeCommand(_executor, _commandBuilder);
         }
     }
 }
