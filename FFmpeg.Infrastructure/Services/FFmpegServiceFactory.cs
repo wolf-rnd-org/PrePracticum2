3d01f11ba53ef44e79b264e94e1e30d91043728d
efrat-stinberg <efrat90906@gmail.com>
2025-07-30 23:40:11 +0300

    Fix build errors by resolving merge conflict markers in FFmpegServiceFactory.cs

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Ffmpeg.Command;
+using Ffmpeg.Command;
 using Ffmpeg.Command.Commands;
 using FFmpeg.Core.Models;
 using FFmpeg.Infrastructure.Commands;
@@ -29,6 +28,7 @@ namespace FFmpeg.Infrastructure.Services
         ICommand<AudioMixModel> CreateMixAudioCommand();
         ICommand<ReverseVideoModel> ReverseVideoCommand();
         ICommand<ResizeModel> CreateResizeCommand();
+        ICommand<BrightnessContrastModel> CreateBrightnessContrastCommand(); 
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -116,108 +116,10 @@ namespace FFmpeg.Infrastructure.Services
         {
             return new ResizeCommand(_executor, _commandBuilder);
         }
-    }
-=======
-using Ffmpeg.Command;
-using Ffmpeg.Command.Commands;
-using FFmpeg.Core.Models;
-using FFmpeg.Infrastructure.Commands;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace FFmpeg.Infrastructure.Services
-{
-    public interface IFFmpegServiceFactory
-    {
-        ICommand<WatermarkModel> CreateWatermarkCommand();
-        ICommand<ThumbnailModel> CreateThumbnailCommand();
-        ICommand<BlurEffectModel> CreateBlurEffectCommand();
-        ICommand<ReplaceAudioModel> CreateReplaceAudioCommand();
-        ICommand<TimestampModel> CreateTimestampCommand();
-        ICommand<MergeVideosModel> CreateMergeVideosCommand();
-        ICommand<ConvertAudioModel> CreateConvertAudioCommand();
-        ICommand<AnimatedTextModel> CreateAnimatedTextCommand();
-        ICommand<GreenScreenModel> CreateGreenScreenCommand();
-        ICommand<ColorFilterModel> CreateColorFilterCommand(); 
-        ICommand<AudioMixModel> CreateMixAudioCommand();
-        ICommand<ReverseVideoModel> ReverseVideoCommand();
-        ICommand<ResizeModel> CreateResizeCommand();
-        ICommand<BrightnessContrastModel> CreateBrightnessContrastCommand();
-    }
-    public class FFmpegServiceFactory : IFFmpegServiceFactory
-    {
-        private readonly FFmpegExecutor _executor;
-        private readonly ICommandBuilder _commandBuilder;
-        public FFmpegServiceFactory(IConfiguration configuration, ILogger logger = null)
-        {
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string ffmpegPath = Path.Combine(baseDirectory, "external", "ffmpeg.exe");
 
-            bool logOutput = bool.TryParse(configuration["FFmpeg:LogOutput"], out bool log) && log;
-
-            _executor = new FFmpegExecutor(ffmpegPath, logOutput, logger);
-            _commandBuilder = new CommandBuilder(configuration);
-        }
-        public ICommand<WatermarkModel> CreateWatermarkCommand()
-        {
-            return new WatermarkCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ThumbnailModel> CreateThumbnailCommand()
-        {
-            return new ThumbnailCommand(_executor, _commandBuilder);
-        }
-        public ICommand<BlurEffectModel> CreateBlurEffectCommand()
-        {
-            return new BlurEffectComand(_executor, _commandBuilder);
-        }
-        public ICommand<ReplaceAudioModel> CreateReplaceAudioCommand()
-        {
-            return new ReplaceAudioCommand(_executor, _commandBuilder);
-        }
-        public ICommand<TimestampModel> CreateTimestampCommand()
-        {
-            return new TimestampCommand(_executor, _commandBuilder);
-        }
-        public ICommand<MergeVideosModel> CreateMergeVideosCommand()
-        {
-            return new MergeVideosCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ConvertAudioModel> CreateConvertAudioCommand()
-        {
-            return new ConvertAudioCommand(_executor, _commandBuilder);
-        }
-        public ICommand<AnimatedTextModel> CreateAnimatedTextCommand()
-        {
-            return new AnimatedTextCommand(_executor, _commandBuilder);
-        }
-        public ICommand<GreenScreenModel> CreateGreenScreenCommand()
-        {
-            return new GreenScreenReplacerCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ColorFilterModel> CreateColorFilterCommand()
-        {
-            return new ColorFilterCommand(_executor, _commandBuilder);
-        }
-        public ICommand<AudioMixModel> CreateMixAudioCommand()
-        {
-            return new MixAudioCommand(_executor, _commandBuilder, new Logger());
-        }
-        public ICommand<ReverseVideoModel> ReverseVideoCommand()
-        {
-            return new ReverseVideoCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ResizeModel> CreateResizeCommand()
-        {
-            return new ResizeCommand(_executor, _commandBuilder);
-        }
         public ICommand<BrightnessContrastModel> CreateBrightnessContrastCommand()
         {
             return new BrightnessContrastCommand(_executor, _commandBuilder);
         }
     }
->>>>>>> 6e5fafbdcccaaba9a60d96414db9aaf0faec03e6
 }
