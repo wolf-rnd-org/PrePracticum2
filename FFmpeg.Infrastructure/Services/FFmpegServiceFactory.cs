eca95de616ca454e0bd967462215f6500ca3de08
tamar mizrachi <t0583279405@gmail.com>
2025-09-07 12:07:00 +0300

    pre2

---
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -0,0 +1,48 @@
+﻿using Ffmpeg.Command;
+using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Commands;
+using Microsoft.Extensions.Configuration;
+
+
+namespace FFmpeg.Infrastructure.Services
+{
+    public interface IFFmpegServiceFactory
+    {
+        ICommand<WatermarkModel> CreateWatermarkCommand();
+        ICommand<ReverseVideoModel> CreateReverseVideoCommand();
+        ICommand<VideoCompressionModel> CreateVideoCompressionCommand();
+
+    }
+
+    public class FFmpegServiceFactory : IFFmpegServiceFactory
+    {
+        private readonly FFmpegExecutor _executor;
+        private readonly ICommandBuilder _commandBuilder;
+
+        public FFmpegServiceFactory(IConfiguration configuration, ILogger logger = null)
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string ffmpegPath = Path.Combine(baseDirectory, "external", "ffmpeg.exe");
+
+            bool logOutput = bool.TryParse(configuration["FFmpeg:LogOutput"], out bool log) && log;
+
+            _executor = new FFmpegExecutor(ffmpegPath, logOutput, logger);
+            _commandBuilder = new CommandBuilder(configuration);
+        }
+
+        public ICommand<WatermarkModel> CreateWatermarkCommand()
+        {
+            return new WatermarkCommand(_executor, _commandBuilder);
+        }
+        public ICommand<ReverseVideoModel> CreateReverseVideoCommand()
+        {
+            return new ReverseVideoCommand(_executor, _commandBuilder);
+        }
+        public ICommand<VideoCompressionModel> CreateVideoCompressionCommand()
+        {
+            return new VideoCompressionCommand(_executor, _commandBuilder);
+        }
+
+    }
+}
